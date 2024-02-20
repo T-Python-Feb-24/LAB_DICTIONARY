335f04baefe645a94f46eccaed0e0b454d5693e5
jmail <jamailotb15@gmail.com>
2024-02-20 13:45:15 +0300

    updated

--- dictionary.py
+++ dictionary.py
@@ -1,25 +1,24 @@
-
 #number_phone ()
 number_phone2 = {
-"amal" :"0568323222" ,
-"mohammeg" :"0522222232"  ,
- "khadijah" :"0532335983" , 
- "abbullah" :"0545341144" ,
- "rawab" : "0545534556" ,
- "faisal" : "0560664566 ",
-  "layla" : "0567917077"
- } 
-number_phone2 = input("write number")
-if not number_phone2.isdigit() or len (number_phone2) !=10:
-    print("sorry the number is not found")
-elif number_phone2 in number_phone2 :
-    print ("this is invalid number")
-else : number_phone2
-print("this is invalid number")
+"0568323222" :"amal" ,
+"0522222232" :"mohamme"  ,
+ "0532335983" :"khadijah" , 
+ "0545341144" :"abbullah" ,
+ "0545534556" : "rawab" ,
+ "0560664566" : "faisal ",
+  "0567917077" : "layla"
+}
+userPhone = input("write number")
+if not userPhone.isdigit() or len (userPhone) !=10:
+    print("sorry the number is invalid")
+elif number_phone2.get(userPhone)== None:
+    print('not found')
+else:
+    print('this phone number belong to: ',number_phone2.get(userPhone))
 #Q2
 a  =[5,0,34,9,13,8]
 n = len (a)
-j = 0
+j = 0 
 for i in range(n):
     if a[i] != 0:
         a[j], a[i] =a[i] ,a [j]
